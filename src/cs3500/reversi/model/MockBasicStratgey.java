0464d955a9bbb3afa7d9d989f10156a680db6dea
zdcar <zdcarpenter@gmail.com>
2023-11-12 20:37:01 -0500

    mock model test

--- src/cs3500/reversi/model/MockBasicStratgey.java
+++ src/cs3500/reversi/model/MockBasicStratgey.java
@@ -7,7 +7,7 @@ public class MockBasicStratgey extends BasicReversi{
   @Override
   List<Hex> getCellsToFlip(int row, int col) {
     List<Hex> list = new ArrayList<>();
-    for (int i = 0; i < row+col; i++){
+    for (int i = 0; i < row+col; i++) {
       list.add(new ReversiCell(0,0,0));
     }
     return list;
